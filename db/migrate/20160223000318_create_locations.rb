cf036bd522a3c4870d24d61244d5d36739a7640e
smesays <smesays@users.noreply.github.com>
2016-03-07 09:00:24 -0800

    Update 20160223000318_create_locations.rb

--- db/migrate/20160223000318_create_locations.rb
+++ db/migrate/20160223000318_create_locations.rb
@@ -1,3 +1,6 @@
+# create a migration from the command line using the following command: 'rails g migration create_locations'
+# edit the migration to ensure the locations table has columns for name, description, tag, coordinates
+# run the migration from the command line by typing 'rake db:migrate'
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
