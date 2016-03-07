2077d23051d368918b7c2b7e5ed6b7606fc4a4de
smesays <smesays@users.noreply.github.com>
2016-03-07 08:58:14 -0800

    Update location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
+# create a file in the app/models directory named 'location.rb'
+# create an Location class in the new model file that inherits from ActiveRecord::Base
 class Location < ActiveRecord::Base
 end
