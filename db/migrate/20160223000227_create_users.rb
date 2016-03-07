033f8fc99f83dd8f09b1ae54ca01932c182b332b
smesays <smesays@users.noreply.github.com>
2016-03-07 08:59:23 -0800

    Update 20160223000227_create_users.rb

--- db/migrate/20160223000227_create_users.rb
+++ db/migrate/20160223000227_create_users.rb
@@ -1,3 +1,6 @@
+# create a migration from the command line using the following command: 'rails g migration create_users'
+# edit the migration to ensure the users table has columns for first_name, last_name, email, password_hash, and password_salt
+# run the migration from the command line by typing 'rake db:migrate'
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
