8f0999bb4f3c1abf52d59bfb3d196dda07a6bc0f
smesays <smesays@users.noreply.github.com>
2016-03-07 08:59:56 -0800

    Update 20160223000302_create_events.rb

--- db/migrate/20160223000302_create_events.rb
+++ db/migrate/20160223000302_create_events.rb
@@ -1,3 +1,6 @@
+# create a migration from the command line using the following command: 'rails g migration create_events'
+# edit the migration to ensure the events table has columns for name, description, start_date, end_date
+# run the migration from the command line by typing 'rake db:migrate'
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
