a2078f979c397e768ede710fab17e43fb32d544d
smesays <smesays@users.noreply.github.com>
2016-03-07 08:57:42 -0800

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
+# create a file in the app/models directory named 'event.rb'
+# create an Event class in the new model file that inherits from ActiveRecord::Base
 class Event < ActiveRecord::Base
 end
